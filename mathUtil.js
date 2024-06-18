a37cb93379641e48215d6106f84a2fffd7bd4a40
ningningedhacker <ning2idhacker@gmail.com>
2024-06-18 17:43:13 +0300

    Add files via upload

---
+++ mathUtil.js
@@ -0,0 +1,10 @@
+export const PI= 3.14;
+export function getCircumferece(radius) {
+    return 2 *PI *radius;
+}
+export function getArea(radius){
+    return PI *radius*radius;
+}
+export function getVolume(radius) {
+    return 4*radius*radius;
+}
